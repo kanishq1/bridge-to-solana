6a12ae4c83c8db1d1c768783528b5133717565ec
kanishq1 <kanishq.khandelwal@devrev.ai>
2023-06-12 00:20:09 +0530

    added transfer page

---
+++ src/components/footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+        <div className="flex justify-between items-center text-[#999A9A] bottom-0">
+            <div className="flex gap-x-12 items-center">
+                <p>wormholenetwork.com</p>
+                <p>Maintained By Yash and Kanishq</p>
+            </div>
+            <p>Powered By: Wormhole</p>
+        </div>
+    );
+};
+
+export default Footer;
---
+++ src/pages/transfer.js
@@ -0,0 +1,10 @@
+import WormholeBridge from "@wormhole-foundation/wormhole-connect";
+import React from "react";
+
+export default function Transfer() {
+    return (
+        <>
+            <WormholeBridge />
+        </>
+    );
+}
