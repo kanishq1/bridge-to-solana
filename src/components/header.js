da6c448952d152b941f8c4073ad1e9ef62e89f76
kanishq1 <kanishq.khandelwal@devrev.ai>
2023-07-26 00:39:20 +0530

    fit and finish

--- src/components/header.js
+++ src/components/header.js
@@ -5,15 +5,15 @@ const Header = () => {
         <div className="w-full flex justify-between">
             <div className="flex justify-between items-center w-full">
                 <a href="/" className="flex items-center gap-x-2">
-                    <img className="opacity-60 w-14" src="/solanaBridgeLogo.svg" />
-                    <div className="text-base">
-                        <p>SOLANA</p>
-                        <p>BRIDGE</p>
-                    </div>
+                    <img className="h-10" src="/solanaBridgeLogo.svg" />
                 </a>
-                <div className="gap-x-8 flex items-center">
-                    <a href="/transfer">Transfer</a>
-                    <a href="/faqs">FAQs</a>
+                <div className="gap-x-8 flex items-center sm:text-lg text-sm font-jakarata">
+                    <a className="hover:opacity-100 opacity-70" href="/transfer">
+                        Transfer
+                    </a>
+                    <a className="hover:opacity-100 opacity-70" href="/faqs">
+                        FAQs
+                    </a>
                 </div>
             </div>
         </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -80,18 +80,16 @@ export default function Home() {
             <Header />
             <div className="flex flex-col justify-center items-center text-center xl:w-1/2 lg:w-3/4 mx-auto gap-y-12 pt-32">
                 <h1 className="font-medium text-6xl font-jakarata">Bridge between Solana and the world.</h1>
-                <p className="text-[#999A9A] text-lg">
+                <p className="text-white font-light text-xl">
                     The Solana Bridge enables seamless asset transfers between Solana and rest of the chains using
                     Wormhole.
                 </p>
-                <div className="">
-                    <a
-                        href="/transfer"
-                        className="bg-white text-black py-4 px-16 rounded font-jakarata hover:bg-[#14F195] ease-linear delay-50 transition-all"
-                    >
-                        GO TO TRANSFER
-                    </a>
-                </div>
+                <a
+                    href="/transfer"
+                    className="bg-white text-black py-4 px-16 rounded font-jakarata hover:bg-[#14F195] ease-linear delay-50 transition-all"
+                >
+                    GO TO TRANSFER
+                </a>
                 <div className="flex flex-col gap-y-8">
                     <p className="flex items-center justify-center gap-x-2">
                         Supported Chains <img src="/online-icon.svg" className="h-6" />
@@ -100,12 +98,12 @@ export default function Home() {
                 </div>
             </div>
             <div class="max-w-xl h-0.5 my-16 mx-auto bg-gradient-to-r from-transparent via-gray-500 to-transparent"></div>
-            <div className="text-white flex justify-center items-center text-center flex-col gap-y-2 mb-12">
+            <div className="text-white flex justify-center items-center text-center flex-col gap-y-4 mb-12 font-jakarata">
                 <div>
                     <h3 className="text-3xl">Ecosystem Bridges</h3>
                 </div>
                 <div>
-                    <p className="text-lg">Swap to and From Solana via the Ecosystem Bridges</p>
+                    <p className="text-xl font-light">Swap to and From Solana via the Ecosystem Bridges</p>
                 </div>
             </div>
             <div>
